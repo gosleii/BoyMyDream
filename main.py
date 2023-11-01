3f28c58c59c630783d92894a03fd36e3ef3f1e09
Pavel Sura <sleii@inbox.ru>
2023-11-01 23:27:46 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,98 @@
+import telebot
+from telebot import custom_filters
+from telebot import StateMemoryStorage
+from telebot.handler_backends import StatesGroup, State
+
+state_storage = StateMemoryStorage()
+bot = telebot.TeleBot("6365794940:AAHqX_T5eLMqlLRXuFFS29CpoVqWGiZGqOc",
+                      state_storage=state_storage, parse_mode='Markdown')
+
+
+class PollState(StatesGroup):
+    name = State()
+    age = State()
+
+
+class HelpState(StatesGroup):
+    wait_text = State()
+
+
+text_poll = "Хочу непременно познакомиться"
+text_button_1 = "Мои увлечения"
+text_button_2 = "Мой ВК"
+text_button_3 = "Мой ТГ"
+
+menu_keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_poll,
+    )
+)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_1,
+    )
+)
+
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_2,
+    ),
+    telebot.types.KeyboardButton(
+        text_button_3,
+    )
+)
+
+
+@bot.message_handler(state="*", commands=['start'])
+def start_ex(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет! Меня зовут *Вадим Куракин*, и Ты находишься в боте, где можешь узнать о моих увлечениях и контактных данных. Давай ты поделишься со мной небольшой информацией о себе, перейдя по кнопке _Хочу непременно познакомиться_?',
+        reply_markup=menu_keyboard)
+
+
+@bot.message_handler(func=lambda message: text_poll == message.text)
+def first(message):
+    bot.send_message(message.chat.id, 'Супер! Как тебя зовут?')
+    bot.set_state(message.from_user.id, PollState.name, message.chat.id)
+
+
+@bot.message_handler(state=PollState.name)
+def name(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+    bot.send_message(message.chat.id, 'А сколько же тебе годиков?')
+    bot.set_state(message.from_user.id, PollState.age, message.chat.id)
+
+
+@bot.message_handler(state=PollState.age)
+def age(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['age'] = message.text
+    bot.send_message(message.chat.id, 'Спасибо, мне было очень притно познакомиться с тобой!',
+                     reply_markup=menu_keyboard)
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: text_button_1 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id,
+                     "Я увлекаюсь программированием, шарю за математику, информатику и физику, так что, обращайся за помощью)",
+                     reply_markup=menu_keyboard)
+
+
+@bot.message_handler(func=lambda message: text_button_2 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "https://vk.com/id334080415", reply_markup=menu_keyboard)
+
+
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "@boy_from_your_dream", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+bot.infinity_polling()
